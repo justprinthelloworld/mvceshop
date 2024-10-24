fc996409e4564e05cb0736e7cc835256aaebc779
justprinthelloworld <justprinthelloworld@gmail.com>
2024-10-24 15:52:11 +0700

    commit for frontend branch
    commit for frontend branch

---
+++ src/stores/user/product.js
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia'
+
+export const useProductStore = defineStore('product', {
+  state: () => ({
+    list: []
+  }),
+  actions: {
+    loadProducts () {
+      const products = localStorage.getItem('admin-products')
+      if (products) {
+        this.list = JSON.parse(products)
+        this.loaded = true
+      }
+    },    
+    filterProducts (searchText) {
+      return this.list.filter(product => product.name.includes(searchText))
+    }
+  }
+})
